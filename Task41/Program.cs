29683bac0ad0ee641db19f8dcfd035d2c5017346
Anastasiya Z <nastya_tim2201@mail.ru>
2022-11-16 16:46:35 +0300

    решили задачу

---
+++ Task41/Program.cs
@@ -0,0 +1,37 @@
+﻿// // Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] FillArrayFromUser(int size)
+{
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+return array;
+}
+
+int CountPositiveNumbers(int[]array)
+{
+	int count = 0;
+	for(int i = 0; i < array.Length; i++)
+	{
+	if(array[i] > 0) count++;
+	}
+	return count;
+}
+
+Console.WriteLine("Укажите, сколько чисел вы хотели бы ввести: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Вводите числа");
+int[] massive = FillArrayFromUser(quantity);
+int posNum = CountPositiveNumbers(massive);
+
+Console.WriteLine($"Вы ввели {posNum} положительных чисел");
+
